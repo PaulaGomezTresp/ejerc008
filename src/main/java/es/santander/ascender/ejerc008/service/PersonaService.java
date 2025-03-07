d22c86d24babe9a50f6cbad4459ec92295ddfaf5
curso02 <niidea@niidea.com>
2025-03-07 12:54:00 +0100

    cambios en persona service

--- src/main/java/es/santander/ascender/ejerc008/service/PersonaService.java
+++ src/main/java/es/santander/ascender/ejerc008/service/PersonaService.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import es.santander.ascender.ejerc008.model.Persona;
+import es.santander.ascender.ejerc008.model.Provincia;
 import es.santander.ascender.ejerc008.repository.PersonaRepository;
+import es.santander.ascender.ejerc008.repository.ProvinciaRepository;
 
 @Service
 @Transactional
@@ -16,8 +18,15 @@ public class PersonaService {
     @Autowired
     private PersonaRepository personaRepository;
 
+    @Autowired
+    private ProvinciaRepository provinciaRepository;
+
+    /*
+     * Los métodos CRUD sobre personaRepository
+     */
 
     public Persona createPersona(Persona persona) {
+        estableceProvincia(persona);
         return personaRepository.save(persona);
     }
 
@@ -38,13 +47,28 @@ public class PersonaService {
             persona.setNombre(personaDetails.getNombre());
             persona.setApellidos(personaDetails.getApellidos());
             persona.setEdad(personaDetails.getEdad());
+            
+            estableceProvincia(personaDetails, persona);
+            
             return personaRepository.save(persona);
         }
-        return null; 
+        return null; // or throw an exception
+    }
+
+    private void estableceProvincia(Persona personaDetails) {
+        estableceProvincia(personaDetails, personaDetails);
+    }
+
+    private void estableceProvincia(Persona personaDetails, Persona persona) {
+        Provincia provincia = null;
+        if (personaDetails.getProvincia()!= null && personaDetails.getProvincia().getId() != null) {
+            provincia = provinciaRepository.findById(personaDetails.getProvincia().getId()).orElse(null);
+            
+        } 
+        persona.setProvincia(provincia);
     }
 
     public void deletePersona(Long id) {
         personaRepository.deleteById(id);
     }
-
 }
