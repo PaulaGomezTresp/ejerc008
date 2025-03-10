a379f9fb66b35d2f24ee4ba0173ef99335ce66f8
curso02 <niidea@niidea.com>
2025-03-10 13:58:41 +0100

    configuracion de autenticacion y autorización

--- src/main/java/es/santander/ascender/ejerc008/controller/ProvinciaController.java
+++ src/main/java/es/santander/ascender/ejerc008/controller/ProvinciaController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,6 +35,7 @@ public class ProvinciaController {
     }
 
     // Read (all)
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public ResponseEntity<List<Provincia>> getAllProvincias() {
         List<Provincia> provincias = provinciaService.getAllProvincias();
