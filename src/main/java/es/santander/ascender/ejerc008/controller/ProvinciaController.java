0f66aa1e48aea7e4fd94b68091d1e1f264d61d6e
PaulaGomezTresp <paulamun_1996@hotmail.com>
2025-03-10 22:53:53 +0100

    cambio autorización de provincia a usuario

--- src/main/java/es/santander/ascender/ejerc008/controller/ProvinciaController.java
+++ src/main/java/es/santander/ascender/ejerc008/controller/ProvinciaController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,7 +34,6 @@ public class ProvinciaController {
     }
 
     // Read (all)
-    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public ResponseEntity<List<Provincia>> getAllProvincias() {
         List<Provincia> provincias = provinciaService.getAllProvincias();
--- src/main/java/es/santander/ascender/ejerc008/controller/UsuarioController.java
+++ src/main/java/es/santander/ascender/ejerc008/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,6 +34,7 @@ public class UsuarioController {
     }
 
     // Read (all)
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public ResponseEntity<List<Usuario>> getAllUsuarios() {
         List<Usuario> usuarios = usuarioService.getAllUsuarios();
